2e4bac09bd4d768976dc8b5c032462e862783eca
React <rusram.nikonchuk@yandex.ru>
2025-02-10 23:00:29 +0300

    chore: configure next

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    images: {
+        domains: ['images.dog.ceo', 'plus.unsplash.com', 's31vla.storage.yandex.net']
+    }
+    /* config options here */
+};
+
+export default nextConfig;
