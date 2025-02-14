317b119f1fee3ab361a933c511d3af4abb032b85
React <rusram.nikonchuk@yandex.ru>
2025-02-14 15:34:29 +0300

    feat: add animation for the home page

---
+++ src/app/components/anima/anima.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './anima.module.css'
+
+const Anima = () => {
+    return (
+        <div className={styles.content}>
+            <div className={styles.cube}><p className={styles.pInCube}>Бизнес десант</p></div>
+        </div>
+    );
+};
+
+export default Anima;
