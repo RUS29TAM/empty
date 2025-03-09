64632ab01f11815efe75e62ed2a918588f5ea4a5
React <rusram.nikonchuk@yandex.ru>
2025-03-09 15:51:05 +0300

    chore: configure next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ['images.dog.ceo', 'plus.unsplash.com', 's31vla.storage.yandex.net', '4.downloader.disk.yandex.ru', 'downloader.disk.yandex.ru', 'images.unsplash.com', "disk.yandex.ru"]
+        domains: ['images.dog.ceo', 'plus.unsplash.com', 's31vla.storage.yandex.net', '4.downloader.disk.yandex.ru', 'downloader.disk.yandex.ru', 'images.unsplash.com', "disk.yandex.ru", "streaming.disk.yandex.net"]
     }
     /* config options here */
 };
