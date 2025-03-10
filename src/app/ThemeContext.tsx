943fcb4fc75dac2e2cfb2062646eeb2187a6e732
React <rusram.nikonchuk@yandex.ru>
2025-03-10 18:06:39 +0300

    feat: add ThemeContext.tsx

---
+++ src/app/ThemeContext.tsx
@@ -0,0 +1,55 @@
+'use client';
+import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+
+interface ThemeProviderProps {
+    children: ReactNode;
+}
+
+interface ThemeContextProps {
+    isDarkTheme: boolean;
+    toggleTheme: () => void;
+    // selectedTopic: string;
+    // setSelectedTopic: (topic: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+    // const [selectedTopic, setSelectedTopic] = useState<string>('economy');
+    const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false); // Стартовое значение
+
+    useEffect(() => {
+        // Проверяем тему в localStorage после монтирования
+        if (typeof window !== 'undefined') {
+            const savedTheme = localStorage.getItem('theme') || 'light';
+            setIsDarkTheme(savedTheme === 'dark');
+            document.body.classList.toggle('dark', savedTheme === 'dark');
+        }
+    }, []);
+
+    const toggleTheme = () => {
+        const newTheme = isDarkTheme ? 'light' : 'dark';
+        setIsDarkTheme(!isDarkTheme);
+        localStorage.setItem('theme', newTheme);
+        document.body.classList.toggle('dark');
+    };
+
+    const contextValue: ThemeContextProps = {
+        isDarkTheme, toggleTheme,
+        // selectedTopic, setSelectedTopic,
+    };
+
+    return (
+        <ThemeContext.Provider value={contextValue}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = (): ThemeContextProps => {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error('useTheme must be used within a ThemeProvider');
+    }
+    return context;
+};
