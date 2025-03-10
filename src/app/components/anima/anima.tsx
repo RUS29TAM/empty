28e1e632448e66a7966af08b50d039f2f03a056f
React <rusram.nikonchuk@yandex.ru>
2025-03-10 17:56:41 +0300

    chore: change bg

--- src/app/components/anima/anima.tsx
+++ src/app/components/anima/anima.tsx
@@ -4,7 +4,7 @@ import styles from './anima.module.css'
 const Anima = () => {
     return (
         <div className={styles.content}>
-            <div className={styles.cube}><p className={styles.pInCube}>Бизнес десант</p></div>
+            <div className={styles.cube}><div className={styles.pInCube}></div></div>
         </div>
     );
 };
