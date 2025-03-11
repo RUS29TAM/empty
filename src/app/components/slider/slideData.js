0ad5c9b1f84e791e70b9d8305cee8c98c8ff2ff6
React <rusram.nikonchuk@yandex.ru>
2025-03-12 01:05:48 +0300

    fix: configure src data

--- src/app/components/slider/slideData.js
+++ src/app/components/slider/slideData.js
@@ -1,51 +1,51 @@
 const slideData = [
     {
-        src: 'https://3.downloader.disk.yandex.ru/preview/2493346cf68ba4f8ab753991ff3324ff0bb2ea05577232deab7230c59488178e/inf/1fb50AV_BC1GVYi7FyZ3nDyWvwAf8Iztis287S6-HVQ9f3F4HWdJDeB0-8-RqhigcqIulFC0NJLwjwUPU1xgcA%3D%3D?uid=1130000063415651&filename=DSC04471.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000063415651&tknv=v2&size=1905x911',
+        src: 'https://downloader.disk.yandex.ru/preview/9e114246c61cd7a86b53cb6385f4c6cd63afe9c157b244f51b96fe9c66a68828/67d0e6e7/1fb50AV_BC1GVYi7FyZ3nDyWvwAf8Iztis287S6-HVQ9f3F4HWdJDeB0-8-RqhigcqIulFC0NJLwjwUPU1xgcA%3D%3D?uid=0&filename=DSC04471.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
         description: '510 км от административного центра Архангельскй области',
         title: 'Вельск',
     },
     {
-        src: 'https://3.downloader.disk.yandex.ru/preview/166203a9163886fa46633bf4c353a7c543183f0f06393e9a01b751a986600553/inf/-cbm_3qnA5_SNI5S3jTCv9pvHh1L3wyPWTHHNGY6bL8s_BwjNiX1wEoC4V2TpJe211x241MXIR_6Xt1sdeu2YQ%3D%3D?uid=1130000063415651&filename=DSC04345.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000063415651&tknv=v2&size=1905x911',
+        src: 'https://downloader.disk.yandex.ru/preview/8037ba1f175b3fdfd3160f44872b18d40575efdeccfdb28bb388cfd12820accc/67d0e745/-cbm_3qnA5_SNI5S3jTCv9pvHh1L3wyPWTHHNGY6bL8s_BwjNiX1wEoC4V2TpJe211x241MXIR_6Xt1sdeu2YQ%3D%3D?uid=0&filename=DSC04345.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
         description: '510 км от административного центра Архангельскй области',
         title: 'Вельск',
     },
     {
-        src: 'https://1.downloader.disk.yandex.ru/preview/a9bc552e9011d2a8ccd32f377e40117026d60a76789ac1bd9a4f9bf235aa5597/inf/BmOVwAwjoegERgmFCRYHu5qlPTk0uHYmtahwG-TIKu64LYLT2ChrQeOHbdJTAWrju_H6pq4g4HEOBrLZ7TwFKg%3D%3D?uid=1130000063415651&filename=DSC04338.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000063415651&tknv=v2&size=1905x911',
+        src: 'https://downloader.disk.yandex.ru/preview/7b7d7f0b251f4e79296bb5f724f9a313be0023252928bed4ebfb04adbd813365/67d0e7e6/BmOVwAwjoegERgmFCRYHu5qlPTk0uHYmtahwG-TIKu64LYLT2ChrQeOHbdJTAWrju_H6pq4g4HEOBrLZ7TwFKg%3D%3D?uid=0&filename=DSC04338.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
         description: '510 км от административного центра Архангельскй области',
         title: 'Вельск',
     },
     {
-        src: 'https://3.downloader.disk.yandex.ru/preview/72d53ab1eae15eaeee6b65e93d139e65a63a51cd8ef8ca31968591334eefccb4/inf/_1VJsMiXIOdObzYwSu3BjShsajD-VnhLbXEGGZ54frhGRnASI-zhw86wRKQ1lnXvnWSaIfixc1xOjEkqaNZuMQ%3D%3D?uid=1130000063415651&filename=DSC05211.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000063415651&tknv=v2&size=1905x911',
+        src: 'https://downloader.disk.yandex.ru/preview/6204877692992b52c72fe814eed3e9c006526a344f6f56f3b2186c1eaab77338/67d0e95b/_1VJsMiXIOdObzYwSu3BjShsajD-VnhLbXEGGZ54frhGRnASI-zhw86wRKQ1lnXvnWSaIfixc1xOjEkqaNZuMQ%3D%3D?uid=0&filename=DSC05211.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
         description: '580 км от административного центра Архангельскй области',
         title: 'Няндома',
     },
     {
-        src: 'https://2.downloader.disk.yandex.ru/preview/5e92c3bc548728618701aa73eb70541dd7ba838a0f2193e1440dd13c64fa323f/inf/UUKwJcM3Cxp0wg5TydDUXd8tVI1thejP-hHVIb94643u3A7vXxS1vTJ_XBjDyMvZbLXpJtIYEYrViopqxp5FqA%3D%3D?uid=1130000063415651&filename=DSC05377.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000063415651&tknv=v2&size=1905x911',
+        src: 'https://downloader.disk.yandex.ru/preview/a56fe9825c611cd736f6f29dbe69980cdd9a53ab6e390a6f1290b56395873173/67d0e9ab/UUKwJcM3Cxp0wg5TydDUXd8tVI1thejP-hHVIb94643u3A7vXxS1vTJ_XBjDyMvZbLXpJtIYEYrViopqxp5FqA%3D%3D?uid=0&filename=DSC05377.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
         description: '580 км от административного центра Архангельскй области',
         title: 'Няндома',
     },
     {
-        src: 'https://2.downloader.disk.yandex.ru/preview/4ed803f6b7b1dba8a769cd73c6ac766723267721be664109f10d73e04cc09a86/inf/ljrz24k7Ivq-riKaQ9f8XU22z7ejqKY4B0h2yRNBDxT83cMUlS0WFsyFWYCQw7bdQ5RauThd6wOdGd8pR3pFoQ%3D%3D?uid=1130000063415651&filename=DSC05141.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000063415651&tknv=v2&size=1905x911',
+        src: 'https://downloader.disk.yandex.ru/preview/e33d273466e2cb008966d20777903d071d6cab9a281b815cb3bf050e2fa88e65/67d0e9f0/ljrz24k7Ivq-riKaQ9f8XU22z7ejqKY4B0h2yRNBDxT83cMUlS0WFsyFWYCQw7bdQ5RauThd6wOdGd8pR3pFoQ%3D%3D?uid=0&filename=DSC05141.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
         description: '580 км от административного центра Архангельскй области',
         title: 'Няндома',
     },
     {
-        src: 'https://2.downloader.disk.yandex.ru/preview/6d1f7615cd500f651ba9b53172631d349f49abbedf3359911093af1789cc1db4/inf/IlnG55LUaGIY0b77uilGTvZss1pQp3b_x8zUrcLduCBqL0VAr9JRxDzYJQk1eya45fpioNc2FXNoZ_s20os3Fg%3D%3D?uid=1130000063415651&filename=DSC04669.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000063415651&tknv=v2&size=1905x911',
+        src: 'https://downloader.disk.yandex.ru/preview/5efd679ef427d6a005cc822535d827851cea66d28d0b14653efdb2eadf2599f8/67d0ea65/IlnG55LUaGIY0b77uilGTvZss1pQp3b_x8zUrcLduCBqL0VAr9JRxDzYJQk1eya45fpioNc2FXNoZ_s20os3Fg%3D%3D?uid=0&filename=DSC04669.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
         description: '630 км от административного центра Архангельскй области',
         title: 'Коноша',
     },
     {
-        src: 'https://4.downloader.disk.yandex.ru/preview/b41ac2a090f88a8a65ca502eeb1698c1e02d7b32cb3c9afd0621a50fcd4e28f8/inf/tyLdDG5PwbQHSrlzqFnOLkN482INUqAl4ayLoUeWgJvURVFWAT65xhjew6eqVlNCgYf01ix9_889OTUOSK3Lfg%3D%3D?uid=1130000063415651&filename=DSC09050.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000063415651&tknv=v2&size=1905x911',
+        src: 'https://downloader.disk.yandex.ru/preview/e9cdbf00153736b93e1591fd678a9c1b2e03d97bf581a7ce2d45e28edeb82fe0/67d0ead0/tyLdDG5PwbQHSrlzqFnOLkN482INUqAl4ayLoUeWgJvURVFWAT65xhjew6eqVlNCgYf01ix9_889OTUOSK3Lfg%3D%3D?uid=0&filename=DSC09050.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
         description: '630 км от административного центра Архангельскй области',
         title: 'с. Ильинско-Подомское',
     },
     {
-        src: 'https://4.downloader.disk.yandex.ru/preview/22ba5f5e777a4f037f1f4d4cc691f8f3075e81980801c9c18e389f2e937b6a17/inf/LvTb3mPkDCWp00U321nenyOeWYrKB1eMPFbTxgvH1dZjEwxK3fkDM-JwO17hil1BDlwvPkAeavrnELZGo-CJGg%3D%3D?uid=1130000063415651&filename=DSC09133.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000063415651&tknv=v2&size=1905x911',
+        src: 'https://downloader.disk.yandex.ru/preview/57c5111372d03cc4747fbe9db6f844ad7f66c8bc1c68899c92736eaa1746d020/67d0eb04/LvTb3mPkDCWp00U321nenyOeWYrKB1eMPFbTxgvH1dZjEwxK3fkDM-JwO17hil1BDlwvPkAeavrnELZGo-CJGg%3D%3D?uid=0&filename=DSC09133.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
         description: '600 км от административного центра Архангельскй области',
         title: 'Котлас',
     },
     {
-        src: 'https://2.downloader.disk.yandex.ru/preview/f1abd2f41dfddabeb569b426c3166649af88a9f42c144198615bde6348441de0/inf/UhqPyhZZ83bZxfWFon4llCOeWYrKB1eMPFbTxgvH1dY2Pwy56gD23K_j4OX0VciG0eWZUcsBejeuuMvNkvqbsg%3D%3D?uid=1130000063415651&filename=DSC09137.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=1130000063415651&tknv=v2&size=1905x911',
+        src: 'https://downloader.disk.yandex.ru/preview/8f765a71d1443654890b42c798c8bd177a02a0b49fdd8af85db7031d9e9158fa/67d0eb35/UhqPyhZZ83bZxfWFon4llCOeWYrKB1eMPFbTxgvH1dY2Pwy56gD23K_j4OX0VciG0eWZUcsBejeuuMvNkvqbsg%3D%3D?uid=0&filename=DSC09137.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2048x2048',
         description: '600 км от административного центра Архангельскй области',
         title: 'Котлас',
     },
