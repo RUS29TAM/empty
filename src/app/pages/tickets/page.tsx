a8f9da0c96d0d9f5ff4c8f94c7a650737dea6eea
React <rusram.nikonchuk@yandex.ru>
2025-03-12 10:16:19 +0300

    chore: configure tickets page

--- src/app/pages/tickets/page.tsx
+++ src/app/pages/tickets/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import CountdownTimer from "@/app/components/countdown-timer/countdown-timer";
+// import CountdownTimer from "@/app/components/countdown-timer/countdown-timer";
+import AboutEvent from "@/app/components/about-event";
 
 const Page = () => {
     return (
-        <CountdownTimer />
+        <>
+            <AboutEvent/>
+            {/*<CountdownTimer/>*/}
+        </>
+
 
     );
 };
